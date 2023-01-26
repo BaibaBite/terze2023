22f614aafec86219d4089e8bc8aa80d8df961056
Baiba Bite <baiba.bite@edu.jekabpils.lv>
2023-01-26 13:10:41 +0200

    Čats jau notiek

--- skripti.js
+++ skripti.js
@@ -2,4 +2,11 @@ console.log("tests")
 //alert(1)
 function sutitZinu(){
     console.log("Poga darbojas");
+
+    //( ) nosaukumu raksra CSS stilā
+    let z = document.querySelector("#zina");
+    //lai pārbaudītu vai darbojas 7.r. saturu iekopē console un nospiež enter, ja ziņa laukā kaut kas ir ierakstīts tas tiks izvadīts
+    let logs = document.querySelector(".chataZinas");
+
+    logs.innerHTML = logs.innerHTML + "</br>" + z.value;
 }
