54abd0d7d9b9f79ee26a918c4e1f4d4010ef44b1
Baiba Bite <baiba.bite@edu.jekabpils.lv>
2023-01-27 11:47:50 +0200

    Update skripti.js

--- skripti.js
+++ skripti.js
@@ -1,6 +1,7 @@
 console.log("tests")
 //alert(1)
 
+const API = "https://terzesana2023.baibabite.repl.co"
 //( ) nosaukumu raksra CSS stilā
 let z = document.querySelector("#zina");
 //lai pārbaudītu vai darbojas 7.r. saturu iekopē console un nospiež enter, ja ziņa laukā kaut kas ir ierakstīts tas tiks izvadīts
